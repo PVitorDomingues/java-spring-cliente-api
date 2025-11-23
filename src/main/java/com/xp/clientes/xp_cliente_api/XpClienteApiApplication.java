16aef822b127e3a1231d424c96ba5255eecb4db1
PauloVitor <vitorldomingues@gmail.com>
2025-11-23 14:55:37 -0300

    feat(setup): Inicializa o projeto Spring Boot com dependências básicas (Web, JPA, H2, Lombok).

---
+++ src/main/java/com/xp/clientes/xp_cliente_api/XpClienteApiApplication.java
@@ -0,0 +1,13 @@
+package com.xp.clientes.xp_cliente_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpClienteApiApplication.class, args);
+	}
+
+}
